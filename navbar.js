4d68b56e3f6d0742848b5583cf6748d5176bede5
damiansztajnhof <43851480+damiansztajnhof@users.noreply.github.com>
2019-03-22 17:24:48 +0100

    Add files via upload

---
+++ navbar.js
@@ -0,0 +1,7 @@
+let nav = '<nav><a href="index.html"> Home  </a>' +
+          '<a href="blog.html"> Blog  </a>' +
+          '<a href="quiz.html"> Quiz  </a>';
+
+let elem4 = document.querySelector("nav-menu");
+
+elem4.innerHTML = nav;
---
+++ script.js
@@ -0,0 +1,7 @@
+let button = dokument.QuerySelector('headerblue');
+let header = document.QuerySelector('headerblue')
+Button.addEventListener("click",changecolor)
+function changecolor() {
+    elem.classList.add('headerblue');
+
+}
